48f39e95401dfd00dd80a128c01c4e2240cd6865
Bfiles333 <bryanfiles@yahoo.com>
2026-03-29 09:30:21 -0400

    Adjusted seed data, replaced table columns in seed table setup and removed foreign key constraint.

--- db/init/002_create_raw_tables.sql
+++ db/init/002_create_raw_tables.sql
@@ -1,3 +1,3 @@
-create table if not exists raw.orders (id SERIAL PRIMARY KEY, order_id integer, order_date timestamp, status VARCHAR);
-create table if not exists raw.order_items (id SERIAL PRIMARY KEY, order_id foreign key, product_id foreign key, quantity integer, unit_price NUMERIC(4, 2), discount NUMERIC(4, 2));
-create table if not exists raw.products (id SERIAL PRIMARY KEY, product_sku VARCHAR, product_name VARCHAR, base_price NUMERIC(4, 2));
+create table if not exists raw.orders (id SERIAL PRIMARY KEY UNIQUE, order_date TIMESTAMP WITH TIME ZONE, status VARCHAR);
+create table if not exists raw.products (id SERIAL PRIMARY KEY UNIQUE, product_sku VARCHAR, product_name VARCHAR, base_price NUMERIC(9, 2));
+CREATE TABLE IF NOT EXISTS raw.order_items (id SERIAL PRIMARY KEY UNIQUE,order_id INT,product_sku VARCHAR,quantity INTEGER,unit_price NUMERIC(9, 2),discount NUMERIC(9, 2),FOREIGN KEY (order_id) REFERENCES raw.orders(id));
