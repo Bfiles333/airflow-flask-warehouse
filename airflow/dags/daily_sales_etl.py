38cb7af168ec52e6de15c269db6d77b16a8a74ff
Bfiles333 <bryanfiles@yahoo.com>
2026-04-10 18:17:04 -0400

    cleaned up dag, reduced process down to one dag with proper sequence

--- airflow/dags/daily_sales_etl.py
+++ airflow/dags/daily_sales_etl.py
@@ -27,22 +27,6 @@ def process_daily_discount_sales():
 
 
 with DAG(
-    dag_id="ingest_seed_data",
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # manual trigger for now
-    catchup=False,
-    tags=["etl"],
-) as dag:
-
-    trigger_ingestion = PythonOperator(
-        task_id="seed_raw_tables",
-        python_callable=seed_raw_tables,
-    )
-
-    trigger_ingestion
-
-
-with DAG(
     dag_id="daily_discount_sales_etl",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,  # manual trigger for now
@@ -50,9 +34,14 @@ with DAG(
     tags=["etl"],
 ) as dag:
 
+    trigger_data_ingestion = PythonOperator(
+        task_id="seed_raw_tables",
+        python_callable=seed_raw_tables,
+    )
+
     trigger_etl = PythonOperator(
         task_id="daily_discount_sales",
         python_callable=process_daily_discount_sales,
     )
 
-    trigger_ingestion
+    trigger_data_ingestion >> trigger_etl
